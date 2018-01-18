3e5a43e00bbc508b422b2bd8a4cc2a2f27d315db
Geoff Mower <g.mower@snhu.edu>
2018-01-18 09:19:53 -0500

    New tables to support SalesforceStg queries and changing MSR_Test table names to include their "__c"

---
+++ flyway/sql/V3__SaleforceStg_Migration/V3.5__Change_Baseline_Table_Names.sql
@@ -0,0 +1,11 @@
+sp_rename 'Program', 'Program__c';
+go
+
+sp_rename 'Program_Goal', 'Program_Goal__c';
+go
+
+sp_rename 'Student_Project', 'Student_Project__c';
+go
+
+sp_rename 'Student_Project_Action', 'Student_Project_Action__c';
+go
